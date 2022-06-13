827829f4f3a1184b8140973415d68555d57ec188
MironSamkov <105563238+MironSamkov@users.noreply.github.com>
2022-06-13 17:26:56 +0300

    Update project.py

--- project.py
+++ project.py
@@ -55,7 +55,7 @@ st.pyplot()
 
 with open("admin_level_4.shp", encoding='CP1251') as o:
     russia_adm4 = gpd.read_file(o)
-#russia_adm4 = gpd.read_file("admin_level_4.shp", encoding='CP1251')
+#russia_adm4 = gpd.read_file("admin_level_4.shp", encoding='Windows-1251')
 st.write(russia_adm4.crs)
 map4 = russia_adm4.to_crs("ESRI:102012")
 fig4 = map4.plot()
