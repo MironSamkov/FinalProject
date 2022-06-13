bf082d6a06ef4f3ad54bca81c11b8923f14e30f4
MironSamkov <105563238+MironSamkov@users.noreply.github.com>
2022-06-13 23:51:59 +0300

    Update project.py

--- project.py
+++ project.py
@@ -104,9 +104,9 @@ for i in set(moscow['Subregion']):
 
 neighbor_map = net.get_adj_list()
 
-for node in net.nodes:
+#for node in net.nodes:
 #    node['title'] += ' Neighbors:<br>' + '<br>'.join(neighbor_map[node['id']])
-    node['value'] = len(neighbor_map[node['id']])
+#    node['value'] = len(neighbor_map[node['id']])
     
 
 fig, ax = plt.subplots()
