8700fd1095423876b9bad4909e88009121757134
MironSamkov <105563238+MironSamkov@users.noreply.github.com>
2022-06-13 08:48:56 +0300

    Update project.py

--- project.py
+++ project.py
@@ -10,10 +10,10 @@ from shapely.geometry import Polygon
 import numpy as np
 import requests
 import geopandas
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+#from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service
+#from webdriver_manager.chrome import ChromeDriverManager
+#from selenium.webdriver.common.by import By
 
 
 """
